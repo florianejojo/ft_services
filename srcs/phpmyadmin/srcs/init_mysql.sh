8b9e7116c61bccea8717f75fa32c8fc32e8ce9e5
Floriane Lefebvre <flolefeb@student.42.fr>
2020-11-25 23:14:51 +0100

    wordpress - impossible d'accéder à la base de donnée

---
+++ srcs/phpmyadmin/srcs/init_mysql.sh
@@ -0,0 +1,2 @@
+mysql_install_db --user=mysql --ldata=/var/lib/mysql 
+/usr/bin/mysqld_safe --datadir=/var/lib/mysql &
---
+++ srcs/phpmyadmin_test/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+if [ ! -f /etc/phpmyadmin/config.secret.inc.php ]; then
+    cat > /etc/phpmyadmin/config.secret.inc.php <<EOT
+<?php
+\$cfg['blowfish_secret'] = '$(tr -dc 'a-zA-Z0-9~!@#$%^&*_()+}{?></";.,[]=-' < /dev/urandom | fold -w 32 | head -n 1)';
+EOT
+fi
+
+if [ ! -f /etc/phpmyadmin/config.user.inc.php ]; then
+    touch /etc/phpmyadmin/config.user.inc.php
+fi
+
+chmod 644 /etc/phpmyadmin/*
+
+tar -Jxf /usr/src/phpMyAdmin.tar.xz -C /var/www/html
+
+exec php-fpm
