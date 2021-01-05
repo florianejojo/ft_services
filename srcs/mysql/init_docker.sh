8aafa247522706c2c7ef27c3b3fc8034555dfbd6
Floriane Lefebvre <flolefeb@student.42.fr>
2021-01-05 13:49:18 +0100

    telegraf working wordpress et pma

--- srcs/mysql/init_docker.sh
+++ srcs/mysql/init_docker.sh
@@ -1,7 +1,9 @@
 #!/bin/ash
-nohup ./init_mysql.sh > /dev/null 2>&1 &
+./init_mysql.sh
 
 sed -i 's/skip-networking/#skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
 /usr/bin/mysql_install_db --user=mysql --datadir="/var/lib/mysql"
 /usr/bin/mysqld_safe --datadir="/var/lib/mysql"
+
+/telegraf/usr/bin/telegraf 
 
